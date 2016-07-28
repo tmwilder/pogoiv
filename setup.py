17cc1e7c9b70928b01d40efb2a7ad19fadff977a
Tim Wilder <tmwilder@gmail.com>
2016-07-27 23:08:14 -0700

    Bump minor version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pogoiv',
-    version='1.0.0',
+    version='1.0.1',
     description='Library for calculating possible pokemon GO IVs.',
     url='http://github.com/tmwilder/pogoiv',
     author='Tim Wilder',
