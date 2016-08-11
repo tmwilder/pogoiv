a7bd61fdc629b8b4df6413a62c230787d8e39852
Tim Wilder <tmwilder@gmail.com>
2016-08-10 22:21:14 -0700

    Update to handle utf-8 pokemon names

--- pogoiv/test/test_base_stats.py
+++ pogoiv/test/test_base_stats.py
@@ -1,3 +1,5 @@
+# -*- encoding: utf-8 -*-
+from __future__ import unicode_literals
 import unittest
 from pogoiv.base_stats import BaseStats
 
@@ -9,3 +11,16 @@ class TestBaseStates(unittest.TestCase):
         self.assertEquals(values[BaseStats.BASE_ATTACK], 128)
         self.assertEquals(values[BaseStats.BASE_STAMINA], 78)
         self.assertEquals(values[BaseStats.BASE_DEFENSE], 108)
+
+    def test_base_stats_nidorans(self):
+        base_stats = BaseStats()
+        female_values = base_stats.get_base_stats('Nidoran♀')
+        male_values = base_stats.get_base_stats('Nidoran♂')
+
+        self.assertEquals(female_values[BaseStats.BASE_ATTACK], 100)
+        self.assertEquals(female_values[BaseStats.BASE_STAMINA], 110)
+        self.assertEquals(female_values[BaseStats.BASE_DEFENSE], 104)
+
+        self.assertEquals(male_values[BaseStats.BASE_ATTACK], 110)
+        self.assertEquals(male_values[BaseStats.BASE_STAMINA], 92)
+        self.assertEquals(male_values[BaseStats.BASE_DEFENSE], 94)
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pogoiv',
-    version='1.0.2',
+    version='1.0.3',
     description='Library for calculating possible pokemon GO IVs.',
     url='http://github.com/tmwilder/pogoiv',
     author='Tim Wilder',
